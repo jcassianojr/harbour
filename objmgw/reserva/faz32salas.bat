9e740a0544c02a90ea5096d7d5b3b438f2dd9459
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2021-04-03 22:40:06 -0300

    rename

---
+++ objmgw/reserva/faz32salas.bat
@@ -0,0 +1,17 @@
+SET HB_INSTALL_PREFIX=c:\hb34\
+rem set HB_BIN_INSTALL=c:\hb34\harbour\bin\ rem nao estava compilando contrib deixando o make criar em \hb34\bin\win\mingw 
+rem set HB_LIB_INSTALL=c:\hb34\harbour\lib\ rem nao estava compilando contrib deixando o make criar em \hb34\lib\win\mingw
+set PATH=c:\devprg\hb32mgw93\comp\mingw\bin\;%PATH%
+rem win-make clean install agora mingw32-make.exe
+rem HB_ARCHITECTURE=linux rem autodectec pega o  sistema
+rem HB_COMPILER=gcc rem autodectec pega o compilador peo path
+rem HB_GPM_MOUSE=no
+rem HB_GT_LIB=gtstd
+rem HB_BIN_INSTALL=bin/
+rem HB_LIB_INSTALL=lib/
+rem HB_INC_INSTALL=include/
+rem mingw32-make.exe clean install
+mingw32-make.exe install
+
+
+
