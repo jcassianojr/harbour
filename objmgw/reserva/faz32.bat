26b2477a8ea893f687c95ef7436a7491feb58902
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2024-09-05 11:50:59 -0300

    novos comentarios

--- objmgw/reserva/faz32.bat
+++ objmgw/reserva/faz32.bat
@@ -1,4 +1,4 @@
-rem HB_COMPILER=mingw
+rem HB_COMPILER=mingw o make ja pega o compilador
 SET HB_INSTALL_PREFIX=d:\harbour\
 SET HB_WITH_ADS=d:\harbour\hb3rd\acesdk\
 SET HB_WITH_CURL=d:\harbour\hb3rd\curl\include\
@@ -12,30 +12,41 @@ set HB_WITH_PGSQL=d:\harbour\hb3rd\pgsql\include\
 SET HB_WITH_SSH2=d:\harbour\hb3rd\ssh2\include\
 SET HB_STATIC_CURL=yes
 SET HB_STATIC_OPENSSL=yes
+rem HB_BUILD_CONTRIB_DYN=no
+rem HB_BUILD_DYN=no
+rem HB_BUILD_SHARED=no
+rem HB_BUILD_STRIP=all
+rem HB_LANG=EN
+
+rem HB_WITH_BZIP2   hbbz2/hbbz2.hbp                  # uses: bz2 (locally hosted)
+rem HB_WITH_EXPAT   hbexpat/hbexpat.hbp              # uses: expat (locally hosted)
+rem HB_WITH_LZF     hblzf/hblzf.hbp                  # uses: liblzf (locally hosted)
+rem HB_WITH_MINILZO hbmlzo/hbmlzo.hbp                # uses: libmlzo (locally hosted)
+rem HB_WITH_MXML    hbmxml/hbmxml.hbp                # uses: minixml (locally hosted)
+rem HB_WITH_MINIZIP hbmzip/hbmzip.hbp                # uses: minizip (locally hosted)
+rem HB_WITH_XDIFF   hbxdiff/hbxdiff.hbp              # uses: libxdiff (locally hosted)
+rem hbbz2io/hbbz2io.hbp              # uses: bz2 (locally hosted)
+rem hbhpdf/hbhpdf.hbp                # uses: libhpdf (locally hosted)
+rem hbsqlit3/hbsqlit3.hbp            # uses: sqlite3 (locally hosted)
+rem sddsqlt3/sddsqlt3.hbp            # uses: sqlite3 (locally hosted
 rem HB_WITH_QT
 rem HB_WITH_RABBITMQ
 rem HB_WITH_BLAT
-rem HB_WITH_BZIP2
 rem HB_WITH_CAIRO
 rem HB_WITH_CUPS
-rem HB_WITH_EXPAT
 rem HB_WITH_GD
 rem HB_WITH_GS
 rem HB_WITH_ZLIB
 rem HB_WITH_PNG
 rem HB_WITH_LIBHARU
 rem HB_WITH_ICU
-rem HB_WITH_LZF
 rem HB_WITH_LIBMAGIC
-rem HB_WITH_MINILZO
-rem HB_WITH_MXML
-rem HB_WITH_MINIZIP
 rem HB_WITH_ODBC
-rem HB_WITH_OPENSSL
 rem HB_WITH_TINYMT
 rem HB_WITH_XDIFF
 rem HB_WITH_LIBYAML
-rem set PATH=d:\harbour\comp\mingw\bin\;d:\harbour\BIN\;%PATH%
+
+rem set PATH=d:\harbour\comp\mingw\bin\;d:\harbour\BIN\;%PATH% call abaixo set os caminhos
 call hb32mgw12
 win-make install
 REM deixando aqui como referencia win-make install e migw32-make sao equivalentes 
--- objmgw/reserva/faz64.bat
+++ objmgw/reserva/faz64.bat
@@ -8,7 +8,7 @@ rem     HB_WITH_gd          - build components dependent on gd (hbgd)
 rem     HB_WITH_mysql       - build components dependent on mysql (hbmysql, sddmy)
 rem     HB_WITH_odbc        - build components dependent on odbc (hbodbc, sddodbc)
 rem     HB_WITH_pgsql       - build components dependent on pgsql (hbpgsql, sddpg)
-rem     HB_WITH_ocalzlib   - build local copy of zlib library
+rem     HB_WITH_localzlib   - build local copy of zlib library
 rem     HB_WITH_localpcre   - build local copy of pcre library
 rem     --whit inclui --whitout nao inclui
 rem     --without x11      - do not build components dependent on x11 (gtxwc)
@@ -16,35 +16,36 @@ rem     --without curses   - do not build components dependent on curses (gtcrs)
 rem     --without slang    - do not build components dependent on slang (gtsln)
 rem     --without gpllib   - do not build components dependent on GPL 3rd party code
 rem     --without gpm      - build components without gpm support (gttrm, gtsln, gtcrs)
-
+rem HB_WITH_BZIP2   hbbz2/hbbz2.hbp                  # uses: bz2 (locally hosted)
+rem HB_WITH_EXPAT   hbexpat/hbexpat.hbp              # uses: expat (locally hosted)
+rem HB_WITH_LZF     hblzf/hblzf.hbp                  # uses: liblzf (locally hosted)
+rem HB_WITH_MINILZO hbmlzo/hbmlzo.hbp                # uses: libmlzo (locally hosted)
+rem HB_WITH_MXML    hbmxml/hbmxml.hbp                # uses: minixml (locally hosted)
+rem HB_WITH_MINIZIP hbmzip/hbmzip.hbp                # uses: minizip (locally hosted)
+rem HB_WITH_XDIFF   hbxdiff/hbxdiff.hbp              # uses: libxdiff (locally hosted)
+rem hbbz2io/hbbz2io.hbp              # uses: bz2 (locally hosted)
+rem hbhpdf/hbhpdf.hbp                # uses: libhpdf (locally hosted)
+rem hbsqlit3/hbsqlit3.hbp            # uses: sqlite3 (locally hosted)
+rem sddsqlt3/sddsqlt3.hbp            # uses: sqlite3 (locally hosted
 rem HB_WITH_QT
 rem HB_WITH_RABBITMQ
 rem HB_WITH_BLAT
-rem HB_WITH_BZIP2
-rem build components dependent on cairo (hbcairo)
 rem HB_WITH_CAIRO
 rem HB_WITH_CUPS
-rem HB_WITH_EXPAT
 rem HB_WITH_GD
 rem HB_WITH_GS
 rem HB_WITH_ZLIB
 rem HB_WITH_PNG
 rem HB_WITH_LIBHARU
 rem HB_WITH_ICU
-rem HB_WITH_LZF
 rem HB_WITH_LIBMAGIC
-rem HB_WITH_MINILZO
-rem HB_WITH_MXML
-rem HB_WITH_MINIZIP
 rem HB_WITH_ODBC
-rem HB_WITH_OPENSSL
 rem HB_WITH_TINYMT
 rem HB_WITH_XDIFF
 rem HB_WITH_LIBYAML
 
-rem HB_COMPILER=mingw
+rem HB_COMPILER=mingw detectado pelo make
 SET HB_INSTALL_PREFIX=d:\harbour\
-rem set PATH=d:\harbour\comp\mingw64\bin\;d:\harbour\BIN\;%PATH%
 rem build components dependent on ads (rddads)
 SET HB_WITH_ADS=d:\harbour\hb3rd\acesdk-x64\
 rem build components dependent on libcurl (hbcurl)
@@ -57,6 +58,7 @@ SET HB_WITH_SSH2=d:\harbour\hb3rd\ssh2-x64\include
 SET HB_STATIC_CURL=yes
 SET HB_STATIC_OPENSSL=yes
 
+rem set PATH=d:\harbour\comp\mingw64\bin\;d:\harbour\BIN\;%PATH% abaixo com o call
 call hb64mgw12.bat
 win-make install
 
