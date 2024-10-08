ad7ba22e1922e2c0e329c8a64a5fec98198029d7
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2024-10-08 15:25:29 -0300

    caminhos

---
+++ objmgw/reserva/harbourbuild/hb64mgw12.bat
@@ -0,0 +1,10 @@
+@echo off
+SET HB_ARCHITECTURE=win
+set HB_COMPILER=mingw64
+set HB_PATH=d:\devprg\hb64mgw12
+SET HB_MINGW=d:\devprg\hb64mgw12\comp\mingw64
+rem SET HB_WITH_ADS=d:\hb64\hb3rd\acesdk-x64\
+SET INCLUDE_DIR=%HB_PATH%\include;%HB_MINGW%\include\
+SET LIB_DIR=%HB_PATH%\lib\win\mingw\;%HB_MINGW%\lib\
+set PATH=%HB_PATH%\bin\;%HB_MINGW%\bin\
+
