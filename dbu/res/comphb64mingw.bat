60049eeaeab72496ad2e72fc37457c776ed504b0
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2024-10-12 15:44:04 -0300

    zips antigos

--- dbu/res/comphb64mingw.bat
+++
@@ -1,2 +0,0 @@
-call d:\DEVPRG\hb64mgw12\hb64mgw12
-hbmk2.exe dbu64.hbp -odbu64.exe
--- objmgw/reserva/hb32mgw12.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET HB_ARCHITECTURE=win
-set HB_COMPILER=mingw
-set HB_PATH=d:\devprg\hb32mgw12
-SET HB_MINGW=d:\devprg\hb32mgw12\comp\mingw
-SET INCLUDE_DIR=%HB_PATH%\include;%HB_MINGW%\include\
-SET LIB_DIR=%HB_PATH%\lib\win\mingw\;%HB_MINGW%\lib\
-set PATH=%HB_PATH%\bin\;%HB_MINGW%\bin\
