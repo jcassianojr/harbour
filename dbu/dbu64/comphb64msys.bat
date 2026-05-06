b4f4253bdfb35bcacbf7278d2a8acb78b592e677
jcassiano <47321671+jcassianojr@users.noreply.github.com>
2026-05-06 16:57:30 -0300

    teste e script mysis

--- dbu/dbu64/comphb64msys.bat
+++ dbu/dbu64/comphb64msys.bat
@@ -1,2 +1,7 @@
 call d:\devprg\hb\hb64msys.bat
 hbmk2.exe dbu64.hbp -odbu64.exe
+rem mt.exe -manifest dbu.exe.manifest -outputresource:dbu.exe;#1
+rem windres --preprocessor=cat dbu.rc -o dbu_res.o
+rem windres --preprocessor=cat d:\develop\harbour\dbu\dbu64\dbu.rc
+rem c:\devprg\msys64\mingw64\bin\windres.exe --no-cpp dbu.rc -o dbu_res.o
+rem c:\devprg\msys64\mingw64\bin\windres.exe dbu.rc dbu_res.o
