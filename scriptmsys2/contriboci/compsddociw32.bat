0f0c42ba992de61f53fab9b1392223c93f10cb96
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2026-05-27 14:38:29 -0300

    versoes de teste

---
+++ scriptmsys2/contriboci/compsddociw32.bat
@@ -0,0 +1,6 @@
+set HB_WITH_OCIlib=d:\harbour\hb3rd\oci\include\
+call d:\DEVPRG\hb\hb32msys
+hbmk2.exe sddoci.hbp
+set HB_WITH_OCIlib=c:\harbour\hb3rd\oci\include\
+call c:\DEVPRG\hb\hb32msys_C.BAT
+hbmk2.exe sddoci.hbp
---
+++ scriptmsys2/contriboci/compsddociw64.bat
@@ -0,0 +1,6 @@
+set HB_WITH_OCIlib=d:\harbour\hb3rd\oci-x64\include\
+call d:\DEVPRG\hb\hb64msys
+hbmk2.exe sddoci.hbp
+set HB_WITH_OCIlib=c:\harbour\hb3rd\oci-x64\include\
+call c:\DEVPRG\hb64\hb64msys_c.bat
+hbmk2.exe sddoci.hbp
