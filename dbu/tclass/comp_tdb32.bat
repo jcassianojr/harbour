9238cf32ff0a6fa5c33ff458ed53052aab368ff0
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2026-06-25 17:47:08 -0300

    Add Tdbclass build files and reference dbutclass.prg
    Introduce tclass build targets and centralize tclass source inclusion. Renamed tclassdbu.prg -> dbu/dbutclass.prg and moved dbuads.prg -> dbu/doc/dbuads.txt. Added Tdbclass.hbp, Tdbclasslib32.hbp, Tdbclasslib64.hbp, comp_tdb32.bat, comp_tdb64.bat and autogenerated tdbclass.hbx. Updated multiple dbu/*.hbp project files to include c:\develop\harbour\dbu\dbutclass.prg (with a small header comment) so hbmk2 can build the Tdbclass library for 32/64-bit targets.

---
+++ dbu/tclass/comp_tdb32.bat
@@ -0,0 +1,3 @@
+call \devprg\hb\hb32msys.bat
+call \devprg\hb\hb32msys_c.bat
+hbmk2.exe Tdbclasslib32.hbp
---
+++ dbu/tclass/comp_tdb64.bat
@@ -0,0 +1,4 @@
+call d:\devprg\hb64\hb64msys.bat
+call c:\devprg\hb64\hb64msys_C.bat
+rem hbmk2.exe Tdbclass.hbp
+hbmk2.exe Tdbclasslib64.hbp
