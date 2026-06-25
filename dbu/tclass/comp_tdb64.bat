4dd8e42b9e15b78129c80203cac6a95aa68a1948
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2026-06-25 18:17:04 -0300

    reordenacao de caminhos

--- dbu/tclass/comp_tdb64.bat
+++ dbu/tclass/comp_tdb64.bat
@@ -1,3 +1,5 @@
+set HB_WITH_MYSQL=c:\harbour\hb3rd\mysql-x64\include\
+set HB_WITH_PGSQL=c:\harbour\hb3rd\pgsql-x64\include\
 call d:\devprg\hb64\hb64msys.bat
 call c:\devprg\hb64\hb64msys_C.bat
 rem hbmk2.exe Tdbclass.hbp
